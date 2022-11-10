3516402f78f3813214e0661105ff7c6565118f0a
Melvin Kempe <k3mp314@gmail.com>
2022-11-10 16:09:44 +0100

    Added more JS
    Co-authored-by: SamuelZainali <SamuelZainali@users.noreply.github.com>
    Co-authored-by: Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
    Co-authored-by: Jennifer Dahlgren <jenniferdahlgren@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -1,41 +1,86 @@
 //Global variables
 
+let array = [35, 29, 23, 47]
+
 let btnLower; //Button to decrease amount of donuts
 let btnHigher; //Button to increase amount of donuts
 let priceLabel; //Label to show total price
 let amountLevel; //Label to show total amount
-let newPrice;
+let newPrice; //Variable to show new price
+let standardPrice; 
+let newAmount; //Variable to show new amount of donuts
 
 function init(){
 
     //Declares variables 
     btnLower = document.querySelector("#decreaseBtn");
     btnHigher = document.querySelector("#increaseBtn");
+    standardPrice = document.querySelector("#donut-price");
     priceLabel = document.querySelector("#tot-price");
     amountLevel = document.querySelector("#tot-amount");
     
     //Calling functions
-    btnLower.addEventListener("click", test);
-    btnHigher.addEventListener("click", test2);
-    console.log("hej");
-    console.log(priceLabel.innerText)
+    btnLower.addEventListener("click", reduceTotDonut);
+    btnHigher.addEventListener("click", increaseTotDonut);
+    console.log(standardPrice.innerText)
 } //End init
 
 
-function test(){
-    newPrice = Number(priceLabel.innerText);
+function reduceTotDonut(){
+
+    newPrice = priceLabel + standardPrice;
+
+
+    newPrice = Number(priceLabel.innerText); //New price equal to priceLabel. Transform priceLabel from string to number
+    console.log(newPrice); 
+    newPrice /= 2; //Takes the number from the label and divides it by two
     console.log(newPrice);
-    newPrice /= 2;
-    console.log(newPrice);
-    priceLabel.innerHTML = newPrice;
+
+    /*if(newPrice <= 35) {
+        btnLower.setAttribute("disabled", true);
+    } */
+    
+    newPrice.innerHTML = newPrice; //Writes out the new number to the label    
+
+    newAmount = Number(amountLevel.innerText);
+    newAmount --;
+    amountLevel.innerHTML = newAmount;
 }
 
-function test2(){
-    newPrice = Number(priceLabel.innerText);
+function increaseTotDonut(){
+    let secondPrice = Number(priceLabel);
+
+    newPrice = Number(standardPrice.innerText);
     console.log(newPrice);
-    newPrice *= 2;
+
+    secondPrice = newPrice + priceLabel;
+    let thirdPrice = secondPrice + newPrice;
+    console.log(thirdPrice);
+    console.log(secondPrice)
+
+    priceLabel.innerHTML = thirdPrice;
+
+    /*newPrice = priceLabel + standardPrice; //New price equal to priceLabel. Transform priceLabel from string to number
     console.log(newPrice);
-    priceLabel.innerHTML = newPrice;
+    newPrice = newPrice + newPrice; //Takes the number from the label and divides it by two
+    console.log(newPrice);
+
+    newPrice.innerHTML = newPrice; //Writes out the new number to the label
+
+    if(newPrice >= 35) {
+        checkPrice();
+    } else {
+        btnLower.setAttribute("disabled", true)
+    }*/
+
+    newAmount = Number(amountLevel.innerText);
+    newAmount ++;
+    amountLevel.innerHTML = newAmount;
+
+}
+
+function checkPrice() {
+        btnLower.removeAttribute("disabled", true);
 }
 
 init();
