79ebea4a2895e949295266b517e120d3de53fc44
Melvin Kempe <k3mp314@gmail.com>
2022-11-11 12:55:36 +0100

    Fixed JS to add and remove donuts

--- script/script.js
+++ script/script.js
@@ -27,56 +27,24 @@ function init(){
 
 
 function reduceTotDonut(){
-
-    newPrice = priceLabel + standardPrice;
-
-
-    newPrice = Number(priceLabel.innerText); //New price equal to priceLabel. Transform priceLabel from string to number
-    console.log(newPrice); 
-    newPrice /= 2; //Takes the number from the label and divides it by two
+    newPrice = Number(standardPrice.innerText);
     console.log(newPrice);
-
-    /*if(newPrice <= 35) {
-        btnLower.setAttribute("disabled", true);
-    } */
-    
-    newPrice.innerHTML = newPrice; //Writes out the new number to the label    
-
     newAmount = Number(amountLevel.innerText);
+    if(newAmount -1 < 0) {
+        return;
+    }
     newAmount --;
     amountLevel.innerHTML = newAmount;
+    priceLabel.innerHTML = newAmount * newPrice;
 }
 
 function increaseTotDonut(){
-    let secondPrice = Number(priceLabel);
-
     newPrice = Number(standardPrice.innerText);
     console.log(newPrice);
-
-    secondPrice = newPrice + priceLabel;
-    let thirdPrice = secondPrice + newPrice;
-    console.log(thirdPrice);
-    console.log(secondPrice)
-
-    priceLabel.innerHTML = thirdPrice;
-
-    /*newPrice = priceLabel + standardPrice; //New price equal to priceLabel. Transform priceLabel from string to number
-    console.log(newPrice);
-    newPrice = newPrice + newPrice; //Takes the number from the label and divides it by two
-    console.log(newPrice);
-
-    newPrice.innerHTML = newPrice; //Writes out the new number to the label
-
-    if(newPrice >= 35) {
-        checkPrice();
-    } else {
-        btnLower.setAttribute("disabled", true)
-    }*/
-
     newAmount = Number(amountLevel.innerText);
     newAmount ++;
     amountLevel.innerHTML = newAmount;
-
+    priceLabel.innerHTML = newAmount * newPrice;
 }
 
 function checkPrice() {
