26d115673a0e0185dd1747db2a9944ce1e9f07e3
Melvin Kempe <k3mp314@gmail.com>
2022-11-30 14:28:31 +0100

    Started working to sort donuts by rating

--- script/script.js
+++ script/script.js
@@ -245,8 +245,13 @@ function showDonuts() {  //Function to display what is in the array/the donuts
     </section>
     `;
   }
+  for (let i = 0; i < donuts.rating; i++) {
+    rating += `<div class="ratingContainer">fa fa-star checked</div>`
+    }
+    console.log(donuts[i].rating);
   showShoppingCart();
   initButtons();
+
 }
 
 
