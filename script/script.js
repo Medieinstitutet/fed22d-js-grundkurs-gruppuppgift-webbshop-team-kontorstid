028c59def314b71380005e2e5796d2faefd9a3b9
Melvin Kempe <k3mp314@gmail.com>
2022-12-02 11:52:37 +0100

    Added sorting by category

--- script/script.js
+++ script/script.js
@@ -207,7 +207,7 @@ function changeOpacity() {
 function swapImages(fadeOut, fadeIn) {
   const img1X = firstImageOnTop ? img1 : img2;
   const img2X = firstImageOnTop ? img2 : img1;
-  
+
   img1X.setAttribute("src", images[fadeOut].url);
   img2X.setAttribute("src", images[fadeIn].url);
   opacityTimer = setInterval(changeOpacity, (fadeTimeInSec * 1000) / 25);
@@ -301,6 +301,10 @@ function updateSorting(e) {
   if (selectedSortingValue === "donutRating") {
     sortAfterRating();
   }
+
+  if (selectedSortingValue === "donutCategory") {
+    sortAfterCategory();
+  }
 }
 
 function sortAfterName() {
@@ -313,10 +317,10 @@ function sortAfterName() {
     }
     return 0;
   });
-  console.table(donuts);
   showDonuts();
   initButtons();
 }
+
 
 function sortAfterLowPrice() {
   donuts.sort((donut1, donut2) => {
@@ -327,6 +331,7 @@ function sortAfterLowPrice() {
   initButtons();
 }
 
+
 function sortAfterHighPrice() {
   donuts.sort((donut1, donut2) => {
     return donut2.price - donut1.price;
@@ -336,6 +341,7 @@ function sortAfterHighPrice() {
   initButtons();
 }
 
+
 function sortAfterRating() {
   donuts.sort((donut1, donut2) => {
     return donut2.rating - donut1.rating;
@@ -344,6 +350,25 @@ function sortAfterRating() {
   showDonuts();
   initButtons();
 }
+
+
+function sortAfterCategory() {
+  donuts.sort((donut1, donut2) => {
+    if (donut1.category > donut2.category) {
+      return 1
+    } else if (donut1.category < donut2.category) {
+      return -1
+    } else {
+      return 0
+    }
+    
+      
+  });
+
+  showDonuts();
+  initButtons();
+}
+
 
 function calculateTotalPrice() {
   const monday = new Date();
